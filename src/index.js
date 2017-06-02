89b4fa8330f1a3640323b3d91be220a3c502c7cd
Scott Savage <snsavage@gmail.com>
2017-06-02 10:16:31 -0400

    Pass all lab specs

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import App from './App';
 
 // we wrap store in a function for testing purposes
 export function configureStore(){
-  return createStore(manageBand, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+  return createStore(manageBand, 
+    window.__REDUX_DEVTOOLS_EXTENSION__ 
+    && window.__REDUX_DEVTOOLS_EXTENSION__());
 };
 
 ReactDOM.render(
