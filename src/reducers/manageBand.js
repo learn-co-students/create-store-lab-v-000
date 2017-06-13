c5bc3e434be928b66980005ab3089e4e504e4ce8
Alex Glover <alexanderbglover@gmail.com>
2017-06-13 16:27:55 -0400

    Done.

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,2 +1,16 @@
-export default function manageUsers(state, action){
+export default function manageBand(state = {
+  users: []
+}, action) {
+  console.log(action)
+  switch(action.type) {
+
+    case 'ADD_USER':
+      console.log("Current state.users length %s", state.users.length)
+      console.log("updating state.users length to %s", state.users.length + 1)
+      return Object.assign({}, state, {users: state.users.concat(action.user)})
+
+    default:
+      return state
+  }
+
 }
