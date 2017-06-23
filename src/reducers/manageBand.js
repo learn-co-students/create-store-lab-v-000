17a4b1fef360b76f9e3a49d95b1ee271d586f691
Phreemason <github email address>
2017-06-23 17:19:19 -0400

    Done.

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -2,9 +2,8 @@ let id = 0
 export default function manageUsers(state = {users: []}, action){
 	switch(action.type){
 		case 'ADD_USER':
-		  id++
-		  
-		  return state.users = [Object.assign({}, state.users, action.user, id: id)]
+		  id++  
+		  return {users: [Object.assign({}, state.users, action.user, id: id)]}
 		default:
 		  return state;
 	}
