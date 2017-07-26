f5147642cddd4031f440fcbd8b5712f9a9b48bc2
Ray Mulligan <rmullig2@optonline.net>
2017-07-25 20:55:06 -0400

    Done.

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,2 +1,10 @@
-export default function manageUsers(state, action){
+export default function manageUsers(state = { users: [] }, action){
+    
+    switch(action.type) {
+        case 'ADD_USER':
+            state.users.push(action.user);
+            return state;
+        default:
+            return state;
+    }
 }
