eb9c195e45b114fbc6d293e4eea8d6f1f4cda2f2
Sergio Mendoza <smendoza787@gmail.com>
2017-08-02 07:51:30 -0700

    Done.

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,10 +1,60 @@
 import React, { Component } from 'react';
 
 class UserInput extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: '',
+      hometown: '',
+    };
+  }
+
+  handleUserChange(event) {
+    this.setState({
+      username: event.target.value,
+    });
+  }
+
+  handleHometownChange(event) {
+    this.setState({
+      hometown: event.target.value,
+    });
+  }
+
+  handleOnSubmit(event) {
+    event.preventDefault();
+    this.props.store.dispatch({
+      type: 'ADD_USER',
+      user: {
+        username: this.state.username,
+        hometown: this.state.hometown,
+      },
+    });
+  }
+
   render() {
     return(
       <div>
-        user input
+        <form onSubmit={(event) => this.handleOnSubmit(event)}>
+          <label>
+            Username:
+            <input
+              type="text"
+              value={this.state.username}
+              onChange={(event) => this.handleUserChange(event)}
+            />
+          </label>
+          <label>
+            Hometown:
+            <input
+              type="text"
+              value={this.state.hometown}
+              onChange={(event) => this.handleHometownChange(event)}
+            />
+          </label>
+          <input type="submit" />
+        </form>
       </div>
     );
   }
