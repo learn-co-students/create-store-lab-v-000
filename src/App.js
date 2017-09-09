62886679439f6a07ada13bac662e23873babf373
Howard DeVennish <howarddevennish@gmail.com>
2017-09-09 15:24:45 -0400

    copy in old code for reference

--- src/App.js
+++ src/App.js
@@ -12,3 +12,24 @@ export class App extends Component {
 };
 
 export default App;
+
+// import './App.css';
+//
+// class App extends Component {
+//
+//   handleOnClick(){
+//     this.props.store.dispatch({
+//       type: 'INCREASE_COUNT',
+//     })
+//   }
+//   render() {
+//     return (
+//       <div className="App">
+//         <button onClick={(event) => this.handleOnClick(event)}>Click</button>
+//         <p>{this.props.store.getState().items.length}</p>
+//       </div>
+//     );
+//   }
+// };
+//
+// export default App;
