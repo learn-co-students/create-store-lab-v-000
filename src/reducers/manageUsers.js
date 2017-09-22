04dd362707c18efcb865257c34e2ebace740b080
John Pfingst <jpfingst@outlook.com>
2017-09-21 21:55:03 -0400

    Done.

--- src/reducers/manageUsers.js
+++ src/reducers/manageUsers.js
@@ -1,2 +1,9 @@
-export default function manageUsers(state, action){
+export default function manageUsers(state = {users: []}, action){
+  switch(action.type) {
+    case 'ADD_USER':
+      const user = {hometown: action.user.hometown, username: action.user.username};
+      return Object.assign({}, state, { users: state.users.concat(user) });
+    default:
+      return state;
+  }
 }
