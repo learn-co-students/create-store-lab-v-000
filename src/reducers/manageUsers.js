671258069394b23934930d0ae135de26ecca42bd
Amanda George <github email address>
2017-09-28 10:07:51 -0500

    Done.

--- src/reducers/manageUsers.js
+++ src/reducers/manageUsers.js
@@ -1,2 +1,9 @@
-export default function manageUsers(state, action){
+export default function manageUsers(state={users: [],}, action){
+  switch(action.type) {
+    case 'ADD_USER':
+      const users= state.users.concat(action.user)
+      return {...state, users}
+    default:
+      return state
+  }
 }
