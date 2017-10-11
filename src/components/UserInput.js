70742706f69082e3d59df07ede8cc7cd64446126
RomanJohnson <roman.johnson+git@gmail.com>
2017-10-11 11:24:24 -0400

    Done.

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,10 +1,61 @@
 import React, { Component } from 'react';
 
 class UserInput extends Component {
+  constructor(){
+    super();
+    this.state={
+      username: "",
+      hometown: "",
+    }
+  }
+
+  handleOnSubmit = (event) =>{
+    event.preventDefault();
+    this.props.store.dispatch({
+      type: 'ADD_USER',
+      user: {
+        username: this.state.username, 
+        hometown: this.state.hometown,
+      }
+    })
+  }
+  handleOnChangeUsername = (event) =>{
+    this.setState({
+      username: event.target.value,
+    }, ()=>console.log("username state is now "+this.state.username))
+    }
+  handleOnChangeHometown = (event) =>{
+    this.setState({
+      hometown: event.target.value,
+    },  ()=>console.log("hometown state is now "+this.state.hometown))
+    }
+
   render() {
     return(
       <div>
-        user input
+        <form onSubmit={this.handleOnSubmit}>
+          <label htmlFor="usernameInput">
+            ENTER username:
+          </label>
+          <input
+            id="usernameInput"
+            type="text"
+            value={this.state.username}
+            onChange={this.handleOnChangeUsername}
+          />
+          <br/>
+
+          <label htmlFor="hometownInput">
+            ENTER hometown:
+          </label>
+          <input
+            id="hometownInput"
+            type="text"
+            value={this.state.hometown}
+            onChange={this.handleOnChangeHometown}
+          />
+          <input type="submit"/>
+        </form>
       </div>
     );
   }
--- src/reducers/manageUsers.js
+++ src/reducers/manageUsers.js
@@ -1,2 +1,10 @@
-export default function manageUsers(state, action){
+export default function manageUsers(state={ users: [] }, action){
+  switch(action.type){
+    case 'ADD_USER':
+      console.log("Current State" + state);
+      return Object.assign({}, state, { users: state.users.concat(action.user)});
+
+    default:
+      return state;
+  }
 }
