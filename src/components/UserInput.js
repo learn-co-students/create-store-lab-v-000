93532a79f9341a1376c6dc82d10c533930c2a251
christinechoi <smi77y@gmail.com>
2017-10-18 16:02:03 -0400

    Done.

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -6,23 +6,39 @@ class UserInput extends Component {
     super(props);
 
     this.state = {
-      hometown: ''
+      hometown: '',
+      username: ''
     };
 
   }
 
-  handleClick() {
+  handleSubmit(event) {
+    event.preventDefault();
 
+    this.props.store.dispatch({
+      type: 'ADD_USER',
+      user: {
+        text: this.state.text
+      }
+    });
 
+    this.setState({
+      text: ''
+    });
   }
 
+  handleChange(event) {
+    this.setState({
+      text: event.target.value
+    }); 
+  }
 
   render() {
     return(
       <div>
-        <form onClick={this.handleClick} >
-          Enter User: <input type="text" />
-          <input type="submit" />
+        <form onSubmit={(event) => this.handleSubmit(event)} >
+          Enter User: <input type="text" value={this.state.text} onChange={(event) => this.handleChange(event)}/>
+          <input type="submit"/>
         </form>
       </div>
     );
--- src/reducers/manageUsers.js
+++ src/reducers/manageUsers.js
@@ -1,6 +1,12 @@
-export default function manageUsers(state, action){
-  
-
-
-
+export default function manageUsers(state = {
+  users: []
+}, action) {
+  switch (action.type) {
+    case 'ADD_USER':
+      const user = state.users.last;
+      return { users: state.users.concat(user) }
+    default: 
+      return state;
+  }
 }
+
