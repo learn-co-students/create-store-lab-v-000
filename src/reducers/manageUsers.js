6c3be8b97fd3da4ad69ae16f9140a4baa470dc78
curlywallst <nnoyes@comcast.net>
2017-10-28 15:15:17 -0400

    Done.

--- src/reducers/manageUsers.js
+++ src/reducers/manageUsers.js
@@ -1,7 +1,6 @@
 export default function manageUsers(state={ users: [] }, action){
   switch (action.type) {
     case 'ADD_USER':
-      const user = Object.assign({}, action.user);
       return Object.assign({}, state, { users: state.users.concat(user)});
 
     default:
