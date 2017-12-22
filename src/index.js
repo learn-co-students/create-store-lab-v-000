eeafe9b82701019219b2d79157cf92009b4aa46a
github username <github email address>
2017-12-22 09:44:47 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import manageUsers from './reducers/manageUsers';
 import App from './App';
-
 // we wrap store in a function for testing purposes
 export function configureStore(){
   return createStore(manageUsers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
@@ -11,5 +10,4 @@ export function configureStore(){
 
 ReactDOM.render(
   <App store={configureStore()} />,
-  document.getElementById('root')
-);
+  document.getElementById('root'))
