c0b3ed95a76e897bf56a4baec63fd2f9d6f8c264
Sophia Hubeny <shhubeny@gmail.com>
2017-12-22 21:11:31 +0000

    working

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 
 class UserInput extends Component {
+
+  handleOnsubmit(event) {
+    this.props.store.dispatch({
+      type:'ADD_USER',
+    })
+  }
+
+  constructor(){
+    super();
+    this.state={
+      username: '',
+      hometown:''
+    }
+  }
   render() {
     return(
       <div>
-        user input
+        <form onSubmit={(event) => this.handleOnsubmit(event)}>
+          Username: <input type="text"/>
+          Hometown: <input type="text"/>
+        </form>
+        <input type="submit"/>
       </div>
     );
   }
