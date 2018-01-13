8484ca53f4f65ecd45b2966cf0fe335882bfaf62
Jim Stricker <jmstricker93@gmail.com>
2018-01-13 10:02:43 -0500

    Done.

--- src/reducers/manageUsers.js
+++ src/reducers/manageUsers.js
@@ -1,2 +1,17 @@
-export default function manageUsers(state, action){
+export default function manageUsers(state={
+  users: []
+}, action) {
+  switch(action.type) {
+
+    case 'ADD_USER':
+      console.log('the action is: ')
+      console.log(action)
+      console.log('store state is: ')
+      console.log(state)
+      debugger;
+      state.users.push(action.user)
+      return state
+    default:
+      return state;
+  }
 }
