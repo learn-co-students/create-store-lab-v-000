eacc54db0fed0f695723cb686f80dfdd6e2a5f1f
psnider30 <pbsnider@gmail.com>
2018-02-05 15:56:39 -0800

    Done.

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,10 +1,51 @@
 import React, { Component } from 'react';
 
 class UserInput extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: '',
+      hometown: '',
+    };
+  }
+
+  handleOnChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleOnSubmit(event) {
+    event.preventDefault();
+    this.props.store.dispatch({
+      type: 'ADD_USER',
+      user: this.state,
+    });
+  }
+
   render() {
     return(
       <div>
-        user input
+        <h5>Sign Up</h5>
+        <form onSubmit={(event) => this.handleOnSubmit(event)}>
+          <label>username</label>
+          <input
+            type='text'
+            name='username'
+            value={this.state.username}
+            onChange={(event) => this.handleOnChange(event)}
+            placeholder='username' />
+          <label>hometown</label>
+          <input
+            type='text'
+            name='hometown'
+            value={this.state.hometown}
+            onChange={(event) => this.handleOnChange(event)}
+            placeholder='username'/>
+          <input type='submit' />
+        </form>
       </div>
     );
   }
--- src/reducers/manageUsers.js
+++ src/reducers/manageUsers.js
@@ -1,2 +1,17 @@
-export default function manageUsers(state, action){
+let id = 1;
+
+export default function manageUsers(state = { users: [] }, action) {
+  switch(action.type) {
+    case "ADD_USER":
+      const user = { username: action.user.username, hometown: action.user.hometown };
+      return Object.assign({}, state, {
+        users: state.users.concat(user)
+      });
+      console.log(users)
+    case "DELETE_USER":
+      const users = state.users.filter(user => user.id !== action.id);
+      return Object.assign({}, state, { users });
+    default:
+      return state;
+  }
 }
