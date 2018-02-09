8bca9073ccb70ee8da2ae1be97f0a982f4e01001
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-02-09 01:37:16 -0800

    Done.

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,10 +1,59 @@
 import React, { Component } from 'react';
 
 class UserInput extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      username: "",
+      hometown: "",
+    }
+  }
+
+  handleOnChangeUsername = (event) => {
+    this.setState({
+      username: event.target.value,
+    })
+  }
+
+  handleOnChangeHometown = (event) => {
+    this.setState({
+      hometown: event.target.value,
+    })
+  }
+
+  handleOnSubmit = (event) => {
+    event.preventDefault();
+    this.props.store.dispatch({
+      type: 'ADD_USER',
+      user: {
+        username: this.state.username,
+        hometown: this.state.hometown,
+      }
+    })
+  }
+
   render() {
     return(
       <div>
-        user input
+        <form onSubmit={(event) => this.handleOnSubmit(event)}>
+          <input
+            placeholder="username"
+            name="username"
+            value={this.state.username}
+            onChange={(event) => this.handleOnChangeUsername(event)}
+          />
+          <br />
+          <input
+            type="text"
+            placeholder="hometown"
+            name="hometown"
+            value={this.state.hometown}
+            onChange={(event) => this.handleOnChangeHometown(event)}
+          />
+          <br />
+          <input type="submit"/>
+        </form>
       </div>
     );
   }
--- src/reducers/manageUsers.js
+++ src/reducers/manageUsers.js
@@ -1,2 +1,12 @@
-export default function manageUsers(state, action){
+export default function manageUsers(state = {
+  users: []
+}, action){
+  switch(action.type) {
+    case 'ADD_USER':
+      console.log('adding users')
+      return {...state, users: [...state.users, action.user] }
+    default:
+      return state;
+  }
+
 }
