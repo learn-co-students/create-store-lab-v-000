9c57295ca61c8ee96562ff27cfb7bd6aa1c3ce45
TravisA <github email address>
2018-03-11 12:34:12 -0600

    Done.

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,10 +1,49 @@
 import React, { Component } from 'react';
 
 class UserInput extends Component {
+  constructor(){
+		super()
+		this.state = {
+			username: '',
+			hometown: '',
+		}
+	}
+
+	handleNameChange(event){
+		this.setState({
+			username: event.target.value,
+		})
+	}
+
+	handleHomeChange(event){
+		this.setState({
+			hometown: event.target.value,
+		})
+	}
+
+	handleSubmit(event){
+		event.preventDefault()
+		this.props.store.dispatch({
+			type: 'ADD_USER',
+			user: {
+				username: this.state.username,
+				hometown: this.state.hometown,
+			}
+		})
+
+		this.setState({
+			username: '',
+			hometown: ''
+		})
+	}
   render() {
     return(
       <div>
-        user input
+        <form onSubmit={(event) => this.handleSubmit(event)}>
+	        <input type='text' onChange={(event) => this.handleNameChange(event)} value={this.state.username}/>
+	        <input type='text' onChange={(event) => this.handleHomeChange(event)} value={this.state.hometown}/>
+	        <input type='submit' />
+	      </form>
       </div>
     );
   }
--- src/reducers/manageUsers.js
+++ src/reducers/manageUsers.js
@@ -1,2 +1,9 @@
-export default function manageUsers(state, action){
+export default function manageUsers(state = { users: [] }, action){
+  switch (action.type) {
+  	case 'ADD_USER':
+  	  console.log(state.users.concat(action.user))
+  	  return {users: state.users.concat(action.user)}
+  	default:
+  	  return state
+  }
 }
