d8f3b06fa3ea7a538585b499634d5d58eb674a22
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-05-05 20:09:01 +0000

    Done.

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,10 +1,63 @@
 import React, { Component } from 'react';
 
 class UserInput extends Component {
+  constructor(props) {
+    super(props) 
+    this.state = {
+        hometown: '',
+        username: '',
+       
+    }
+  }
+  
+  handleHomeTown = (e) => {
+    console.log(e.target.value);
+    // debugger
+    this.setState({
+      hometown: e.target.value,
+    });
+  }
+  
+  
+  handleUserName = (e) => {
+    console.log(e.target.value);
+    // debugger
+    this.setState({
+      username: e.target.value,
+    });
+  }
+  
+  handleOnSubmit = (event) => {
+    event.preventDefault();
+    this.props.store.dispatch({
+      type: 'ADD_USER', 
+      user: this.state,
+    });
+    this.setState({
+        username: '',
+        hometown: ''
+    });
+  }
+  
   render() {
     return(
       <div>
         user input
+        <form onSubmit={(event) => this.handleOnSubmit(event)} >
+         
+          <input 
+            type='text' 
+            name='username'
+            value={this.state.username} 
+            onChange={(event) => this.handleUserName(event)} />
+             
+          <input 
+            type='text' 
+            name='hometown'
+            value={this.state.hometown}
+            onChange={(event) => this.handleHomeTown(event)} />
+          <input type="submit" />
+        </form>
       </div>
     );
   }
