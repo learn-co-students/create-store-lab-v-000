f93357ac6c7baf276f1be45e9a41feaa5d7cca80
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-05-25 16:01:03 -0500

    Done.

--- src/reducers/manageUsers.js
+++ src/reducers/manageUsers.js
@@ -1,2 +1,15 @@
-export default function manageUsers(state, action){
+let id = 0;
+
+export default function manageUsers(state = {
+	users: []
+}, action){
+	switch(action.type) {
+		case 'ADD_USER':
+			id++
+			const user = { username: action.user.username, hometown: action.user.hometown };
+			return { users: state.users.concat(user) };
+		default: 
+			return state;
+	}
+
 }
