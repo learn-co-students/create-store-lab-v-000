d9020f86b661a8508f80199566126b31766a19a4
jennianelson <jennifernelson118@gmail.com>
2018-05-25 10:07:02 -0500

    Done.

--- src/reducers/manageUsers.js
+++ src/reducers/manageUsers.js
@@ -1,2 +1,16 @@
-export default function manageUsers(state, action){
+export default function manageUsers(state ={
+  users: []
+}, action) {
+  console.log(action);
+  switch(action.type) {
+    case 'ADD_USER':
+      console.log("Current state.users:", state.users);
+      console.log("Updating state.users:", action.user);
+      // return Object.assign({}, state, { users: state.users.concat(action.user)});
+      return {...state, users: state.users.concat(action.user) }
+    default:
+      console.log("Current state.users:", state.users);
+      return state;
+  }
+  
 }
