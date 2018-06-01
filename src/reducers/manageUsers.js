b631c1fb7fe80688a8ab3ff01d3859b533a88753
AnastasiaBarkett <anastasia.barkett@gmail.com>
2018-05-31 21:39:47 -0400

    Tests passing

--- src/reducers/manageUsers.js
+++ src/reducers/manageUsers.js
@@ -1,2 +1,10 @@
-export default function manageUsers(state, action){
+export default function manageUsers(state = {
+  users: []}, action){
+    switch(action.type){
+      case 'ADD_USER':
+        const user = { username: action.user.username, hometown: action.user.hometown};
+        return Object.assign({}, state, {users: state.users.concat(action.user) });
+      default:
+        return state;
+    }
 }
