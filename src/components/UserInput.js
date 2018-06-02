91967c1425c8462c75726cddf891fba02bc49b9f
chrisjamesr <chrisjames.r@gmail.com>
2018-06-02 08:42:46 -0400

    Done.

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,10 +1,66 @@
 import React, { Component } from 'react';
 
 class UserInput extends Component {
+  constructor(){
+    super()
+
+    this.state= {
+      username: '',
+      hometown:''
+    }
+    this.handleUsernameChange = this.handleUsernameChange.bind(this)
+    this.handleHometownChange = this.handleHometownChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    // this.handleInputChange = this.handleInputChange.bind(this)
+  }
+
+  // handleInputChange(event){
+  //   this.setState({
+  //     ...this.state, [event.target.id]: event.target.value
+  //   })
+  // }
+
+  handleUsernameChange(event){
+    this.setState({
+      ...this.state, 
+      username: event.target.value
+    })
+  }
+  
+  handleHometownChange(event){
+    this.setState({
+      ...this.state, 
+      hometown: event.target.value
+    })
+  }
+
+  handleSubmit(event){
+    event.preventDefault()
+    this.props.store.dispatch({
+      type: 'ADD_USER',
+      user: this.state
+    })
+    this.setState({username:'', hometown:''})
+  }
+
   render() {
     return(
       <div>
-        user input
+      <form onSubmit={this.handleSubmit}> 
+        <input id="username"
+               placeholder="Enter Username"
+               type="text" 
+               value={this.state.username} 
+               onChange={this.handleUsernameChange}
+               />
+        <input id="hometown"
+               placeholder="Enter Hometown"
+               type="text"       
+               value={this.state.hometown}
+               onChange={this.handleHometownChange}
+               />
+        <button>Submit</button>       
+      </form>  
       </div>
     );
   }
--- src/reducers/manageUsers.js
+++ src/reducers/manageUsers.js
@@ -1,2 +1,13 @@
-export default function manageUsers(state, action){
+export default function manageUsers(state ={users:[]}, action){
+  console.log('action: ' + JSON.stringify(action))
+  console.log('state: ' + JSON.stringify(state))
+  switch (action.type){
+    case 'ADD_USER':
+
+      const users = state.users.concat(action.user)
+      console.log('state: ' + JSON.stringify(users))
+      return {users}
+    default:
+      return state
+  }
 }
