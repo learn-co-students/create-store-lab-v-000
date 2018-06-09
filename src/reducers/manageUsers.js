8a9bf55021e0f18893a03b12282ff6d410bdc956
Sophia Varella <sophiavarella@gmail.com>
2018-06-08 17:03:03 -0700

    Done.

--- src/reducers/manageUsers.js
+++ src/reducers/manageUsers.js
@@ -1,2 +1,24 @@
-export default function manageUsers(state, action){
+// export default function manageUsers(state = {
+//   users: [],
+// }, action) {
+//   switch(action.type) {
+//     case 'ADD_USER':
+//       const user = Object.assign({}, action.user)
+//       return { ...state, users: state.users.concat(user) }
+//     default:
+//       return state;
+//   }
+// }
+
+
+export default function manageUsers(state = {
+  users: [],
+}, action) {
+  switch(action.type) {
+    case 'ADD_USER':
+      const user = { username: action.user.username, hometown: action.user.hometown }
+      return { ...state, users: state.users.concat(user) }
+    default:
+      return state;
+  }
 }
