6241d8568ec2c6a5ebbefd59f7af599e2a677651
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-06-15 17:05:07 -0400

    Two tests left to pass.

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 
 class UserInput extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: '',
+      hometown: ''
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      username: event.target.value
+    })
+  }
+
   render() {
     return(
       <div>
-        user input
+        Input: <input onChange={(event) => this.handleChange(event)} />
       </div>
     );
   }
