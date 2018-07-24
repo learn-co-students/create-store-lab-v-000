a61d4deed251f896f11953ad7f45cb1e25acb24b
lawrend <douglas_lawrence@mac.com>
2018-07-24 06:45:26 -0700

    done

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,10 +1,56 @@
 import React, { Component } from 'react';
 
 class UserInput extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: '',
+      hometown: '',
+    };
+  }
+
+  handleOnChange(event, inputType) {
+    if(inputType === "username") {
+      this.setState({
+        username: event.target.value,
+        hometown: this.state.hometown,
+      })
+    } else {
+      this.setState({
+        username: this.state.username,
+        hometown: event.target.value,
+      });
+    }
+  }
+
+  handleOnSubmit(event) {
+    event.preventDefault();
+    this.props.store.dispatch({
+      type: 'ADD_USER',
+      user: {
+        username: this.state.username,
+        hometown: this.state.hometown,
+      },
+    });
+    this.setState({
+      username: '',
+      hometown: '',
+    });
+  }
+
   render() {
-    return(
+    return (
       <div>
-        user input
+      <form onSubmit={(event) => this.handleOnSubmit(event)}>
+      <label>Username: </label>
+      <input type="text" onChange={(event) => this.handleOnChange(event, "username")} value={this.state.username}/>
+      <br></br>
+      <label>Hometown: </label>
+      <input type="text" onChange={(event) => this.handleOnChange(event, "hometown")} value={this.state.hometown}/>
+      <br></br>
+      <input type="submit" value="add user"/>
+      </form>
       </div>
     );
   }
