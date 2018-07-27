2d213dae824cb5e83d3f716df96069ec9deb62c6
R. Harris Haynes <harrishaynes@gmail.com>
2018-07-27 16:08:52 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import UserInput from './components/UserInput';
 
-export class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <div className="App">
@@ -10,5 +10,3 @@ export class App extends Component {
     );
   }
 };
-
-export default App;
--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,13 +1,57 @@
 import React, { Component } from 'react';
 
-class UserInput extends Component {
+export default class UserInput extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      username: '',
+      hometown: ''
+    };
+  }
+
+  handleOnSubmit(e){
+    e.preventDefault();
+    this.props.store.dispatch({
+      type: 'ADD_USER',
+      user: {
+        username: this.state.username,
+        hometown: this.state.hometown
+      }
+    });
+    this.setState({text: ''});
+  }
+
+  handleOnChange(e){
+    if (e.target.name === "username"){
+      this.setState({username: e.target.value, hometown: this.state.hometown});
+    } else {
+      this.setState({hometown: e.target.value, username: this.state.username});
+    }
+  }
+
   render() {
-    return(
+    return (
       <div>
-        user input
+        <form onSubmit={event => this.handleOnSubmit(event)}>
+          <input
+            id="username"
+            name="username"
+            type="text"
+            value={this.state.username}
+            placeholder="Enter username"
+            onChange={event => this.handleOnChange(event)}
+          /><br />
+          <input
+            id="hometown"
+            name="hometown"
+            type="text"
+            value={this.state.hometown}
+            placeholder="Enter hometown"
+            onChange={event => this.handleOnChange(event)}
+          /><br />
+          <input type="submit" />
+        </form>
       </div>
     );
   }
 };
-
-export default UserInput;
